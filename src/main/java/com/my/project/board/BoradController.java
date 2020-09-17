486515a45d7d4f501f8fc65bee3b3360702c5e74
jisung324 <jisung3245@gmail.com>
2020-09-18 03:15:57 +0900

    .

--- src/main/java/com/my/project/board/BoradController.java
+++ src/main/java/com/my/project/board/BoradController.java
@@ -1,5 +1,20 @@
 package com.my.project.board;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/board")
 public class BoradController {
 
+	
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public String boardList() {
+	
+		
+		return "/board/boardList";
+	}
+	
+	
 }
