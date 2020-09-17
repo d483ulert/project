4833c20c0e16b86d32217efbc7e8e0b30f52cfdb
jisung324 <jisung3245@gmail.com>
2020-09-18 03:04:55 +0900

    boardVO

---
+++ src/main/java/com/my/project/board/BoardDAO.java
@@ -0,0 +1,5 @@
+package com.my.project.board;
+
+public class BoardDAO {
+
+}
---
+++ src/main/java/com/my/project/board/BoardVO.java
@@ -0,0 +1,15 @@
+package com.my.project.board;
+
+import java.util.Date;
+import lombok.Data;
+
+@Data
+public class BoardVO {
+	private int bno;
+	private String title;
+	private String content;
+	private String writer;
+	private Date regdate;
+	private Date updatedate;
+	
+}
